9bec56eefbe668dbc95ec9e46dbaae30f20bfc6c
Mario Fučkan <mario.fucchan15@gmail.com>
2025-07-02 04:42:41 +0200

    v2

--- src/lib/server/authProviders.ts
+++ src/lib/server/authProviders.ts
@@ -2,4 +2,4 @@ import { DISCORD_CLIENT_SECRET } from "$env/static/private"
 import { PUBLIC_DISCORD_CLIENT_ID } from "$env/static/public"
 import { Discord } from "arctic"
 
-export const discord = new Discord(PUBLIC_DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, `${location.origin}/auth/callback`)
+export const discord = new Discord(PUBLIC_DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, `${window.location.origin}/auth/callback`)
